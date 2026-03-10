1a762bd0f99265f1b292a8c0663e24d2b1853c82
matheus19ggg <matheus19ggg@gmail.com>
2026-03-10 16:50:56 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+/* Script principal compartilhado (opcional) */
+console.log("Ferramentas Online carregadas com sucesso!");
+
+// Função para formatar números se necessário
+function formatNumber(num) {
+    return new Intl.NumberFormat('pt-BR').format(num);
+}
